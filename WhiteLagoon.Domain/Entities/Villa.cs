abf26795536be8fb760dc34dced5f82a2326e4bc
InfintixITSolutions <infinitixitsolutions@gmail.com>
2024-09-21 16:26:55 +0530

    Villa CRUD

--- WhiteLagoon.Domain/Entities/Villa.cs
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,16 +12,21 @@ namespace WhiteLagoon.Domain.Entities
     {
         public int Id { get; set; }
 
+        [MaxLength(60)]
         public required string Name { get; set; }   
 
         public string? Description { get; set; }
 
-        public double Price { get; set; }
+        [Display(Name = "Price Per Night")]
+        [Range(10,10000)]
+        public double Price { get; set; }  
 
         public int Sqft { get; set; }
 
-        public int Occupancy { get; set; }  
+        [Range(1,10)]
+        public int Occupancy { get; set; }
 
+        [Display(Name = "Image URL")]
         public string? ImageUrl { get; set; }
 
         public DateTime? CreatedDate { get; set; }
---
+++ WhiteLagoon/Controllers/VillaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        /// <summary>
+        /// In the constructor (public VillaController(ApplicationDbContext db)), dependency
+        /// injection is used to pass the ApplicationDbContext object (db) into the controller. This means the
+        /// controller does not need to manually instantiate the database context; it's provided by the ASP.NET Core runtime.
+        /// </summary>
+        /// <param name="db"></param>
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villas = _db.tbl_Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+                ModelState.AddModelError("Name", "You cannot keep same desciption and name");
+            
+            if (ModelState.IsValid)
+            {
+                _db.tbl_Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            Villa? obj = _db.tbl_Villas.FirstOrDefault(v => v.Id == id);
+            if(obj == null)
+                return NotFound();
+            return View(obj);
+        }
+    }
+}
